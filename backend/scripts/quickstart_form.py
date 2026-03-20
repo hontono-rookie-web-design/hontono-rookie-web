ef162dec3ffd0a20358f644de0a131322f31cf58
moku45654 <moku45654@gmail.com>
2026-03-20 21:50:56 +0900

    サービスアカウントを用いたGoogleフォーム作成のテスト（失敗）

--- backend/scripts/quickstart_form.py
+++ backend/scripts/quickstart_form.py
@@ -1,23 +1,27 @@
 import os
 
 from apiclient import discovery
+from google.oauth2.service_account import Credentials
 from httplib2 import Http
 from oauth2client import client, file, tools
 
-SCOPES = "https://www.googleapis.com/auth/forms.body"
+SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly"
 DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
 credentials_path = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
-store = file.Storage("token.json")
-creds = None
-if not creds or creds.invalid:
-  flow = client.flow_from_clientsecrets(credentials_path, SCOPES)
-  creds = tools.run_flow(flow, store)
+# store = file.Storage("token.json")
+# creds = None
+# if not creds or creds.invalid:
+#   flow = client.flow_from_clientsecrets(credentials_path, SCOPES)
+#   creds = tools.run_flow(flow, store)
+
+creds = Credentials.from_service_account_file(credentials_path, scopes=SCOPES)
 
 form_service = discovery.build(
     "forms",
     "v1",
-    http=creds.authorize(Http()),
+    # http=creds.authorize(Http()),
+    credentials=creds,
     discoveryServiceUrl=DISCOVERY_DOC,
     static_discovery=False,
 )
@@ -62,15 +66,20 @@ NEW_QUESTION = {
 }
 
 # Creates the initial form
-result = form_service.forms().create(body=NEW_FORM).execute()
+# result = form_service.forms().create(body=NEW_FORM).execute()
 
-# Adds the question to the form
-question_setting = (
-    form_service.forms()
-    .batchUpdate(formId=result["formId"], body=NEW_QUESTION)
-    .execute()
-)
+form_id = '{form_id}'
+result = form_service.forms().responses().list(formId=form_id).execute()
+print(result)
 
-# Prints the result to show the question has been added
-get_result = form_service.forms().get(formId=result["formId"]).execute()
-print(get_result)
+
+# # Adds the question to the form
+# question_setting = (
+#     form_service.forms()
+#     .batchUpdate(formId=result["formId"], body=NEW_QUESTION)
+#     .execute()
+# )
+
+# # Prints the result to show the question has been added
+# get_result = form_service.forms().get(formId=result["formId"]).execute()
+# print(get_result)
