8a322b663dc681ba67c76f094885c43b4537459c
Ared001 <4ooghtas3@gmail.com>
2026-03-31 00:59:55 +0900

    Update page.tsx

--- 2026spring/frontend/src/app/submissions/songs/rookie/page.tsx
+++ 2026spring/frontend/src/app/submissions/songs/rookie/page.tsx
@@ -1,7 +1,7 @@
 import VideoCard from "@/components/video/VideoCard"
 
 async function getVideos() {
-  const res = await fetch("/api/videos", {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/videos`), {
     cache: "no-store"
   })
 
