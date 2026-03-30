9851ae316e0f9f33fe655f4e828fe2071afe89c7
Ared001 <4ooghtas3@gmail.com>
2026-03-30 22:20:45 +0900

    Update page.tsx

--- 2026spring/frontend/src/app/submissions/songs/rookie/page.tsx
+++ 2026spring/frontend/src/app/submissions/songs/rookie/page.tsx
@@ -1,5 +1,31 @@
-import TBA from "@/components/TBA";
+import VideoCard from "@/components/video/VideoCard"
 
-export default function Page() {
-  return <TBA title="楽曲一覧（ルーキー）" />;
+async function getVideos() {
+  const res = await fetch("/api/videos", {
+    cache: "no-store"
+  })
+
+  return res.json()
+}
+
+export default async function Page() {
+  const videos = await getVideos()
+
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1 style={{ marginBottom: "30px" }}>楽曲一覧（ルーキー）</h1>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))",
+          gap: "20px",
+        }}
+      >
+        {videos.map((video: any, index: number) => (
+          <VideoCard key={index} video={video} />
+        ))}
+      </div>
+    </main>
+  )
 }
