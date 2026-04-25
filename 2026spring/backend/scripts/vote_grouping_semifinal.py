e3c827ffdb44a94a1a5d44002551523f64adee78
shirooymd <yamaori227@gmail.com>
2026-04-25 22:14:33 +0900

    準決勝シート書き込み処理を追加

--- 2026spring/backend/scripts/vote_grouping_semifinal.py
+++ 2026spring/backend/scripts/vote_grouping_semifinal.py
@@ -22,7 +22,6 @@ def main():
     config = utils.load_config()
     tag_config: dict[str, str] = config["tag"]
 
-    ### 2026/4/23~
     ranking_sheet_config = config["spreadsheets"]["ranking_result"]
     ranking_spreadsheetname = ranking_sheet_config["name"]
     preliminary_sheetname = ranking_sheet_config["preliminary_sheet"]
@@ -48,70 +47,18 @@ def main():
     semifinal_group_size = config["vote_grouping"]["group_size"]//2 #準決勝グループ数=予選グループ数の半分
 
     df["準決勝グループID"]=df.apply(calc_semifinal_no,axis=1,args=(semifinal_group_size,)) #準決勝グループID列を作成し更新
-    
+
 
     #シートに書き込み
+    semifinal_sheetname = ranking_sheet_config["semifinal_sheet"]
+    output_sheet = connect_sheet(ranking_spreadsheetname,semifinal_sheetname)
 
+    sheet_client.clear_sheet(output_sheet)  # 既存のデータをクリア
+    sheet_client.update_sheet(output_sheet, df.to_dict(orient='records')) # orient='records'で[{列名: 値}, ...]の形式で辞書を作成
 
-
-        
+    print(f"Successfully updated '{semifinal_sheetname}' in '{ranking_spreadsheetname}' with grouped data.")
 
     
-    ###
-
-    # catalog_sheet_config = config["spreadsheets"]["video_catalog"]
-    # catalog_spreadsheetname = catalog_sheet_config["name"]
-
-    # # for div in ["rookie", "op", "ex"]:
-    # for div in tag_config.keys():
-
-    #     catalog_excluded_sheetname = catalog_sheet_config[f"excluded_{div}_sheet"]
-
-    #     # シートに接続
-    #     excluded_sheet = connect_sheet(
-    #         catalog_spreadsheetname, catalog_excluded_sheetname
-    #     )
-
-    #     # シートからデータを取得
-    #     video_data = sheet_client.fetch_sheet_data(excluded_sheet)
-    #     # print(video_data)
-
-    #     # 取得したデータが空であればスキップ
-    #     if any(video_data) == False:
-    #         print(f"No data found in {catalog_excluded_sheetname}. Skipping.")
-    #         continue
-
-    #     # pandasのDataFrameに変換
-    #     df = pd.DataFrame(video_data)
-    #     # print(df)
-
-    #     # content_idのリストを作成
-    #     content_ids = df["動画ID"].tolist()
-    #     # print(content_ids)
-
-    #     # グループ分け
-    #     seed = config["vote_grouping"]["random_seed"]
-    #     print(f"Using random seed: {seed}")
-    #     grouper = ContentGrouper(content_ids, seed=seed)
-
-    #     # グループ一つあたりの人数（サイズ）によってグループ分け
-    #     group_size = config["vote_grouping"]["group_size"]
-    #     print(f"Grouping by size: {group_size} items per group")
-    #     grouper.group_by_size(group_size)
-
-    #     # DataFrameにグループ番号の列を追加
-    #     df["グループID"] = df["動画ID"].apply(grouper.get_group_id)
-
-    #     print(df)
-
-    #     # グループ分けされたデータを新しいシートに書き込む
-    #     output_spreadsheetname = config["vote_grouping"]["grouped_video_catalog"]["name"]
-    #     output_sheetname = config["vote_grouping"]["grouped_video_catalog"][f"{div}_sheet"]
-    #     output_sheet = connect_sheet(output_spreadsheetname, output_sheetname)
-    #     sheet_client.clear_sheet(output_sheet)  # 既存のデータをクリア
-    #     sheet_client.update_sheet(output_sheet, df.to_dict(orient='records')) # orient='records'で[{列名: 値}, ...]の形式で辞書を作成
-
-    #     print(f"Successfully updated '{output_sheetname}' in '{output_spreadsheetname}' with grouped data.")
 
 if __name__ == "__main__":
     main()
