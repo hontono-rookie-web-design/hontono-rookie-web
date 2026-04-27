e351e4d977cb0ead83f9185b9f724c2a2f670143
shirooymd <yamaori227@gmail.com>
2026-04-27 21:44:03 +0900

    準決勝グループ分け書き込み先を修正

--- 2026spring/backend/scripts/vote_grouping_semifinal.py
+++ 2026spring/backend/scripts/vote_grouping_semifinal.py
@@ -12,8 +12,8 @@ def connect_sheet(spreadsheet_name, sheet_name):
     return sheet_client.connect_sheet(credentials_path, spreadsheet_name, sheet_name)
 
 def calc_semifinal_no(row,size):
-    m=row["グループID"]-1
-    r=row["順位"]-1
+    m=row["予選グループID"]-1
+    r=row["予選順位"]-1
     return (m+r)%size+1
 
 def main():
@@ -25,8 +25,9 @@ def main():
     ranking_sheet_config = config["spreadsheets"]["ranking_result"]
     ranking_spreadsheetname = ranking_sheet_config["name"]
     preliminary_sheetname = ranking_sheet_config["preliminary_sheet"]
-    catalog_semifinal_sheet_config = config["spreadsheets"]["video_catalog_semifinal"]
+    catalog_semifinal_sheet_config = config["vote_semifinal"]["grouped_video_catalog_semifinal"]
     catalog_semifinal_spreadsheetname = catalog_semifinal_sheet_config["name"]
+    catalog_semifinal_sheetname = catalog_semifinal_sheet_config["rookie_sheet"]
     
 
     #予選順位データ取得
@@ -46,25 +47,27 @@ def main():
 
     #データフレーム作成
     df = pd.DataFrame(preliminary_data)
-
+    # グループID→予選グループIDにリネーム
+    df = df.rename(columns={"グループID": "予選グループID"})
     #予選通過ボーダーより上をフィルタリング
     border = config["vote_semifinal"]["semifinal_border"]
     df["順位"] = pd.to_numeric(df["順位"], errors='coerce')
     df = df[df["順位"] <= border].copy()
 
+    df = df.rename(columns={"順位": "予選順位"})
+
     semifinal_group_size = config["vote_grouping"]["group_num"]//2 #準決勝グループ数=予選グループ数の半分
 
-    df["準決勝グループID"]=df.apply(calc_semifinal_no,axis=1,args=(semifinal_group_size,)) #準決勝グループID列を作成し更新
+    df["グループID"]=df.apply(calc_semifinal_no,axis=1,args=(semifinal_group_size,)) #準決勝グループID列を作成し更新
 
 
     #シートに書き込み
-    semifinal_sheetname = ranking_sheet_config["semifinal_sheet"]
-    output_sheet = connect_sheet(ranking_spreadsheetname,semifinal_sheetname)
+    output_sheet = connect_sheet(catalog_semifinal_spreadsheetname, catalog_semifinal_sheetname)
 
     sheet_client.clear_sheet(output_sheet)  # 既存のデータをクリア
     sheet_client.update_sheet(output_sheet, df.to_dict(orient='records')) # orient='records'で[{列名: 値}, ...]の形式で辞書を作成
 
-    print(f"Successfully updated '{semifinal_sheetname}' in '{ranking_spreadsheetname}' with grouped data.")
+    print(f"Successfully updated '{catalog_semifinal_sheetname}' in '{catalog_semifinal_spreadsheetname}' with grouped data.")
 
     
 
