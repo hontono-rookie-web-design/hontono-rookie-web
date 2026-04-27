b59ae6a4698509b285bc9357b6ca5b725267ec66
Ared001 <4ooghtas3@gmail.com>
2026-04-27 19:14:13 +0900

    Update page.tsx

--- 2026spring/frontend/src/app/derivative/Postingform/page.tsx
+++ 2026spring/frontend/src/app/derivative/Postingform/page.tsx
@@ -6,11 +6,11 @@ export default function Page() {
       {/* ヘッダー */}
       <div className="text-center mb-8 w-full max-w-[760px]">
         <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">
-          二次創作 投稿フォーム
+          二次創作 提出フォーム
         </h1>
 
         <p className="text-xs sm:text-sm text-gray-600 mt-2">
-          二次創作作品の投票はこちらから行えます。
+          二次創作作品の提出はこちらから行えます。
         </p>
 
         <div className="mt-4 border-b border-gray-200 w-full" />
@@ -23,7 +23,7 @@ export default function Page() {
           <li>ニコニコ動画にアップロードする場合、「本当のルーキー祭り2026春二次創作」をタグロックしてください。</li>
           <li>noteに投稿する場合、「本当のルーキー祭り2026春二次創作」のタグをつけてください。</li>
           <li>指定のGoogleフォームに投稿内容を入力してください。本サイトに反映されます。投稿者以外が入力しても構いません。</li>
-          <li>※ note投稿者はGoogleフォームへの回答は不要です。</li>
+          <li>note投稿者はGoogleフォームへの回答は不要です。</li>
           <li>商業目的の利用（無許可での販売・グッズ化）は禁止ですが、YouTube等の収益化済み配信での利用は可能です。</li>
           <li>ルーキー歌い手は開催期間内に、ニコニコ動画へ新規投稿する必要があります。</li>
           <li>つまり、期間前にニコニコ動画へ投稿してしまうと、その動画では参加できなくなります。</li>
