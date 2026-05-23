aff2f6bb7bc15cc87897a072318b67c9857261c7
Ared001 <4ooghtas3@gmail.com>
2026-05-23 15:11:48 +0900

    Update derivative.ts

--- 2026spring/frontend/src/lib/derivative.ts
+++ 2026spring/frontend/src/lib/derivative.ts
@@ -1,8 +1,32 @@
 import { fetchFanficSheet } from "@/lib/fetchSheet"
 import { CONFIG } from "@/config/config"
 
-export async function getDerivativeSongs() {
+export async function getDerivativeArrangements() {
   return await fetchFanficSheet(
-    CONFIG.fanficsheets.music.name
+    CONFIG.fanficsheets.arrangements.name
+  )
+}
+
+export async function getDerivativeCoverSongs() {
+  return await fetchFanficSheet(
+    CONFIG.fanficsheets.coversongs.name
+  )
+}
+
+export async function getDerivativeIllustrations() {
+  return await fetchFanficSheet(
+    CONFIG.fanficsheets.illustrations.name
+  )
+}
+
+export async function getDerivativeOthers() {
+  return await fetchFanficSheet(
+    CONFIG.fanficsheets.others.name
+  )
+}
+
+export async function getDerivativeStreams() {
+  return await fetchFanficSheet(
+    CONFIG.fanficsheets.streams.name
   )
 }
