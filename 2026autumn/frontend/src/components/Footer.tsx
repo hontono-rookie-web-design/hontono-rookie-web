62e0a61bff41d9d21daba8970687bc383371e964
Helium-Helippy <ymdmhce@gmail.com>
2026-09-09 01:01:56 +0900

    Crowdfunding.tsx削除とFooter.tsx作成とヴォエさん後援削除

--- 2026autumn/frontend/src/components/Footer.tsx
+++ 2026autumn/frontend/src/components/Footer.tsx
@@ -1,5 +1,7 @@
-export default function PrivacyPolicy() {
-  return (
+
+export default function Footer() {
+    return (
+    <footer className="w-full py-8 text-center text-sm text-slate-400 bg-white">
     <div className="max-w-4xl mx-auto px-6">
       <p className="w-full py-6 text-slate-400 text-center text-sm bg-white">
         当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
@@ -24,5 +26,7 @@ export default function PrivacyPolicy() {
         ページをご覧ください。
       </p>
     </div>
-  );
-}
+        <p>© 2026 本当のルーキー祭り 運営</p>
+    </footer>
+    );
+} 
